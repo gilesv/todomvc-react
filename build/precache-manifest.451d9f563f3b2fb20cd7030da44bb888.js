8377c73377b1f5407ce76e2362bda7fcc582c4f6
gilesv <gilesv@protonmail.com>
2020-10-08 22:09:48 -0300

    First build

---
+++ build/precache-manifest.451d9f563f3b2fb20cd7030da44bb888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ea4bca8e51f36766aae2f7f17ba66d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe1fd2b83fa159f69819",
+    "url": "/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "fe1fd2b83fa159f69819",
+    "url": "/static/js/2.a078edb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a078edb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8675660ed99eb364b2b4",
+    "url": "/static/js/main.dacab9fb.chunk.js"
+  },
+  {
+    "revision": "ea31603b7079f29e139a",
+    "url": "/static/js/runtime-main.a36fca5d.js"
+  }
+]);
