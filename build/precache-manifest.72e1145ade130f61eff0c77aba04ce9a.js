bfff8071d3a9e8a7dd9b9f92cdd53cf078ba778b
gilesv <gilesv@protonmail.com>
2020-10-08 22:19:52 -0300

    Build

---
+++ build/precache-manifest.72e1145ade130f61eff0c77aba04ce9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6279b010a7215122fbfbb64ba645a8",
+    "url": "/todomvc-react/index.html"
+  },
+  {
+    "revision": "fe1fd2b83fa159f69819",
+    "url": "/todomvc-react/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "fe1fd2b83fa159f69819",
+    "url": "/todomvc-react/static/js/2.a078edb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todomvc-react/static/js/2.a078edb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8675660ed99eb364b2b4",
+    "url": "/todomvc-react/static/js/main.dacab9fb.chunk.js"
+  },
+  {
+    "revision": "0dcff5287946db172c32",
+    "url": "/todomvc-react/static/js/runtime-main.d277dd97.js"
+  }
+]);
